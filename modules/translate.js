cabe5b5e280332f661997d4d2cbc6440e1dd5263
Soochaehwa <Soochaehwa@shwa.space>
2022-10-07 12:18:14 +0900

    Update user agent

--- modules/translate.js
+++ modules/translate.js
@@ -20,7 +20,7 @@ export default async function translate(
       referer: "https://translate.google.com/",
       "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
       "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
     };
 
     sourceText = `"${sourceText}"`;
